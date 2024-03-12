a173017def5fbe34aa2b4423268e5bbf0dc14b6d
Karol <a.karoluju97@gmail.com>
2024-03-12 11:51:47 +0000

    Learning to create new variables

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,12 @@
 // This function will be called when the button is clicked
 function showWelcomeMessage() {
     // Get a reference to the message container
-    var messageContainer = document.getElementById("messageContainer");
+    //var messageContainer = document.getElementById("messageContainer");
+    var firstname = document.getElementById("firstname");
     
     // Set the message text to "Welcome!"
-    messageContainer.textContent = "Welcome!";
+    //messageContainer.textContent = "Welcome!";
+    firstname.textContent = "Karol"
 }
 
 // Get a reference to the button element using its ID
